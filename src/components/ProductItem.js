94a027bee3f917d38fce75a84c1351c9b14c2e06
Vladislavkapusta <118127681+Vladislavkapusta@users.noreply.github.com>
2023-10-02 19:57:08 +0300

    Update ProductItem.js
    -

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -3,10 +3,10 @@ import React from 'react';
 const ProductItem = ({ id, name, price, count, onIncrement, onDecrement, onDelete }) => {
 
   return (
-    <div className='card' onDoubleClick={() => onDelete(id)}>
+   count !== 0 && <div className='card' onDoubleClick={() => onDelete(id)}>
       <h1 className='card-title'>{name}</h1>
-      <p className='card-price'>Price: {price}</p>
-      <p className='card-quantity'>Count: {count}</p>
+      <p className='card-price'>Цена: {price}</p>
+      <p className='card-quantity'>В корзине: {count}</p>
       <div>
         <button className='PM' onDoubleClick={(e) => e.stopPropagation()} onClick={() => onIncrement(id)}>+</button>
         <button className='PM' onDoubleClick={(e) => e.stopPropagation()} onClick={() => onDecrement(id)}>-</button>
